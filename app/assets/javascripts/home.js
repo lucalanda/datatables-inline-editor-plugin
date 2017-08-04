3ea1c9fa782b45fd226caefce6ca8276c45b3af6
lucalanda <llucalanda@gmail.com>
2017-08-04 14:31:50 +0200

    a

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,14 @@
-function initializePage() {
-    $('#table').DataTable();
+var table;
 
+function initializePage() {
+    table = $('#table').DataTable();
+
+    $('#table tbody').on('click', 'tr', function (xyz) {
+        var data = table.row( this ).data();
+        console.log(xyz);
+        for (var i in data)
+            console.log(data[i]);
+
+        // alert( 'You clicked on '+data[0]+'\'s row' );
+    } );
 }
