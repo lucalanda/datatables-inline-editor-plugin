969a4e03ba46047efe5f028cee94b0d87d8c804a
lucalanda <llucalanda@gmail.com>
2017-08-08 14:31:43 +0200

    * implemented inline-editing first version

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,22 +1,77 @@
 var table;
 
+const ENTER_KEY_CODE = 13;
+const ESC_KEY_CODE = 27;
+
 function initializePage() {
     table = $('#table').DataTable();
 
-    $('#table tbody').on('click', 'td', function (xyz) {
+    function checkFunction(inputData) {
+        return false;
+    }
+
+    function successFunction(inputData) {
+        alert('success!');
+    }
+
+    function errorFunction(inputData) {
+        alert('error!');
+    }
+
+    setEditableColumns(table, [0, 1], checkFunction, successFunction, errorFunction);
+}
+
+//TODO give and use different functions for different columns enabled
+//TODO disable editor on all columns
+//TODO disable editor on some columns
+
+function setEditableColumns(table, columns, checkFunction, successFunction, errorFunction) {
+    table.on('click', 'td', function() {
         var cell = table.cell(this);
 
         var rowIndex = cell.index().row;
         var columnIndex = cell.index().column;
-        console.log('clicked cell row: ' + rowIndex);
-        console.log('clicked cell column: ' + columnIndex);
+        console.log('row index: ' + rowIndex);
+        console.log('column index: ' + columnIndex);
+
+        if (columns && !columns.includes(columnIndex)) {
+            return;
+        }
 
         var cellData = cell.data();
-        console.log('clicked cell content: "' + cellData + '"');
+        var rowData = table.row(rowIndex).data();
 
         table
             .cell(rowIndex, columnIndex)
-            .data('<p style="color:red">' + cellData + '</p>')
-            .draw(false);
+            .data('<input class="form-control" type="text" id="edit-area">');
+
+        $('#edit-area').focus();
+        $('#edit-area').blur(resetTableRow);
+
+        $('#edit-area').keypress(function (event) {
+            if (event.which === ENTER_KEY_CODE) {
+                var input = $('#edit-area').val();
+
+                if (checkFunction(input)) {
+                    rowData[columnIndex] = input;
+                    table.row(rowIndex).remove();
+                    table.row.add(rowData);
+                    table.draw();
+
+                    successFunction(input);
+                } else {
+                    errorFunction();
+                }
+            } else if (event.which === ESC_KEY_CODE) {
+                resetTableRow();
+            }
+        });
+
+        function resetTableRow() {
+            table
+                .cell(rowIndex, columnIndex)
+                .data(cellData)
+                .draw(false)
+        }
     });
 }
